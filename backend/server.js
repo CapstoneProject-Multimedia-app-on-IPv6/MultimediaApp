b33920cb15f01ad143011c6041c2ec97e71d026b
HuyQuahM <quahhuy03@gmail.com>
2024-10-10 15:32:32 +0700

    Source Code Organization và cập nhật thêm tính năng phát nhạc

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,21 @@
-const express = require('express'); // Import thư viện Express
-const mongoose = require('mongoose'); // Import thư viện Mongoose để tương tác với MongoDB
-const bcrypt = require('bcryptjs'); // Import bcryptjs để mã hóa mật khẩu
-const jwt = require('jsonwebtoken'); // Import jsonwebtoken để tạo và xác minh token
-const cors = require('cors'); // Import CORS để cho phép tương tác giữa các domain khác nhau
+const express = require('express');
+const connectDB = require('./config/db');
 
-const app = express(); // Khởi tạo một ứng dụng Express
-const port = 5000; // Đặt cổng mà server sẽ lắng nghe
+const authRoutes = require('./routes/authRoutes');
+const musicRoutes = require('./routes/musicRoutes');
+
+const app = express();
+const port = 5000;
 
 // Kết nối tới MongoDB
-mongoose.connect('mongodb://localhost:27017/multimedia-app');
+connectDB();
 
-app.use(cors()); // Cho phép truy cập API từ các domain khác
-app.use(express.json()); // Middleware để phân tích các yêu cầu HTTP với dữ liệu JSON
+// Các cấu hình khác cho Express
+app.use(express.json());
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-const User = mongoose.model('User', userSchema);
-
-// Đăng ký người dùng
-app.post('/register', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10); // Mã hóa mật khẩu
-    const user = new User({ email, password: hashedPassword }); // Tạo đối tượng người dùng mới
-    await user.save(); // Lưu vào MongoDB
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    res.status(400).json({ error: 'Failed to create user' });
-  }
-});
-
-// Đăng nhập người dùng
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email }); // Tìm người dùng trong MongoDB theo email
-    if (!user) return res.status(400).json({ error: 'User not found' });
-
-    const isPasswordValid = await bcrypt.compare(password, user.password); // So sánh mật khẩu đã mã hóa
-    if (!isPasswordValid) return res.status(400).json({ error: 'Invalid password' });
-
-    const token = jwt.sign({ id: user._id }, 'secretKey', { expiresIn: '1h' }); // Tạo JWT token
-    res.status(200).json({ token, user: { email: user.email } });
-  } catch (error) {
-    res.status(400).json({ error: 'Login failed' });
-  }
-});
+// Kết nối các route cho tính năng phát nhạc và xác thực người dùng với ứng dụng chính
+app.use('/music', musicRoutes);
+app.use('/auth', authRoutes);
 
 // Khởi động server
 app.listen(port, () => {
