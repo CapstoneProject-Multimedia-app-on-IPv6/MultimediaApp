b11e5688731ad5c3f433c011b172b4d478a52879
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2024-12-21 16:11:38 +0700

    GH action workflow and terraform file

---
+++ Terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "ami_id" {
+  default = "ami-0866a3c8686eaeeba"
+
+
+}
+
+variable "aws_access_key" {
+  default = "ASIASQTZNICCOGYDAXUG"
+}
+variable "aws_secret_key" {
+  default = "lfE90qznVhA4kuumv8m1C45kyLmgqDwtO9CpFD61"
+}
+variable "token" {
+  default = "IQoJb3JpZ2luX2VjENn//////////wEaCXVzLXdlc3QtMiJHMEUCIC9+MYu6tlmu5pPaF7l6B9OC9ftDoaKPxVimka7dPb/7AiEAp6kjTHgOonspDjjf/i1uPdcgaAZFskrQ9IUFxHc7BsMquQIIov//////////ARAAGgwxNzMxMjcwNTc1NDAiDB7mPsL/qu0WB4JT/SqNArOLVkOSsIzATcgAI16ElYhxmW/kHx7Z3mji2DP3h5+Ib2p66z6o2VBpDdYB/ZWb3Ljzlsyvl9MeHpdj31+SlCoJnqM/UsrLs4QEfJd+EWoLNmVDvpQ/99l6KXztCVMJNITI5MUZNaLMUdgMFTTK0mRz+OvV1P3pgYiuxUndgDMysQG3ldX6XAIHq5TVugMbWdmaIXg/Fjx+Z7te6Aes/mQWAp4I3EvdACAxSQHEjtkhVvC3JbOSaUV6+UoOVV89Pn27y94FR0Z87Ud7U0lxGFnCHLz9xjziOrFcP7S3QTc4HK9eRu+0mFiSzr0vgR2dny96VsSEH3uweIgrZ94vOatkkk3rmZilryUDEfOtMOb8mbsGOp0BkKFxuafPAf/Pi0z9eRlX+5SRE6jGFP5rsUNk2LqC7Xhp6jRCO9hSa0DIpjk5omXgH+nG2H+m619vruq+Zht4b4AmvYnUNPuQBsfiDz2+Ql7sjpOpQOQEAZnSYk0QCkCmCvgsE0KtbnbP7ESjvs2VDt02YweHV/dUyx279as6qOmAMHg/LDXpSundnpiLwIzBodnR1Dyq+Jt+dDlGVQ==" 
+}
