06c6cdc3d2150ad9599a3919ab3d987378158b65
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2025-01-02 10:57:00 +0700

    💩

--- Server/src/app.js
+++ Server/src/app.js
@@ -9,6 +9,16 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src 'self' https://35.197.156.82:8090; media-src 'self' https://35.197.156.82:8090;"
+  );
+  next();
+});
+
+
 app.use(cors());
 app.use(cookieParser());
 // Lấy đường dẫn thư mục hiện tại
@@ -18,9 +28,11 @@ app.use(express.static(path.resolve(__dirname, "../build")));
 console.log("thu muc hien tai: ", __dirname);
 
 app.use("/api", router);
-mongoose.connect("mongodb+srv://giahuy:user123@cluster0.fno0x.mongodb.net/phim").then(() => {
-  console.log("Connect to db success");
-});
+mongoose.connect("mongodb+srv://giahuy:user123@cluster0.fno0x.mongodb.net/phim")
+        .then(() => {
+          console.log("Connect to db success");
+              })
+        .catch(err => console.error("Failed to connect to DB:", err));
 app.listen(8089, () => {
   console.log("Server is running 8089 port");
 });
