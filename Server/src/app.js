e8ca8d80b74ea899c70b93763f1e5ec20cac15d0
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2025-01-02 21:42:59 +0700

    🔥

--- Server/src/app.js
+++ Server/src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use((req, res, next) => {
       res.setHeader(
         "Content-Security-Policy",
-        "default-src 'self' blob: https://35.197.156.82:8090 http://localhost:8090; img-src 'self' data: https://35.197.156.82:8090 http://localhost:8090; style-src 'self' 'unsafe-inline';"
+        "default-src 'self' blob: https://35.197.156.82:8090 http://localhost:8090; img-src 'self' data: https://35.197.156.82:8090 http://localhost:8090; media-src 'self' data: https://35.197.156.82:8090 http://localhost:8090; style-src 'self' 'unsafe-inline';"
       );
 
       res.setHeader(
