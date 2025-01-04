1affd00b37485b5e99d722e9e0e6c2aec3c851b0
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2025-01-04 20:52:32 +0700

    c

--- Server/src/app.js
+++ Server/src/app.js
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self' blob: https://35.198.228.50:8090 http://localhost:8090 http://35.197.156.82:8090/; " +
+    "default-src 'self' blob: http://35.198.228.50:8090 http://localhost:8090 http://35.197.156.82:8090/; " +
     "script-src 'self' https://unpkg.com; " + 
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/; " + 
-    "img-src 'self' data: https://35.198.228.50:8090 http://localhost:8090 http://35.197.156.82:8090/; " +
-    "media-src 'self' data: https://35.198.228.50:8090 http://localhost:8090 http://35.197.156.82:8090/; " +
+    "img-src 'self' data: http://35.198.228.50:8090 http://localhost:8090 http://35.197.156.82:8090/; " +
+    "media-src 'self' data: http://35.198.228.50:8090 http://localhost:8090 http://35.197.156.82:8090/; " +
     "worker-src 'self' blob: *;" +
     "font-src 'self' data: https://fonts.gstatic.com;"
   );
@@ -66,5 +66,5 @@ app.listen(8089, () => {
 
 //   res.setHeader(
 //     "Content-Security-Policy-Report-Only",
-//     "default-src 'self' blob: https://35.198.228.50:8090; img-src 'self' data: https://35.198.228.50:8090;"
+//     "default-src 'self' blob: http://35.198.228.50:8090; img-src 'self' data: http://35.198.228.50:8090;"
 // );
