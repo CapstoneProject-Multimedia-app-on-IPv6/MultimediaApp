a2fe825b4187dd4f4fb66f314d5cd37ae91d73d3
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2025-01-14 23:46:23 +0700

    terraform for gcp

--- Terraform/.terraform.lock.hcl
+++ Terraform/.terraform.lock.hcl
@@ -1,24 +1,22 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/hashicorp/aws" {
-  version = "5.70.0"
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "6.16.0"
+  constraints = "6.16.0"
   hashes = [
-    "h1:F0Xy1oGTyjjr4VLvCuscnjuIP26roJhveM87hZIcU3E=",
-    "zh:09cbec93c324e6f03a866244ecb2bae71fdf1f5d3d981e858b745c90606b6b6d",
-    "zh:19685d9f4c9ddcfa476a9a428c6c612be4a1b4e8e1198fbcbb76436b735284ee",
-    "zh:3358ee6a2b24c982b7c83fac0af6898644d1bbdabf9c4e0589e91e427641ba88",
-    "zh:34f9f2936de7384f8ed887abdbcb54aea1ce7b0cf2e85243a3fd3904d024747f",
-    "zh:4a99546cc2140304c90d9ccb9db01589d4145863605a0fcd90027a643ea3ec5d",
-    "zh:4da32fec0e10dab5aa3dea3c9fe57adc973cc73a71f5d59da3f65d85d925dc3f",
-    "zh:659cf94522bc38ce0af70f7b0371b2941a0e0bcad02d17c1a7b264575fe07224",
-    "zh:6f1c172c9b98bc86e4f0526872098ee3246c2620f7b323ce0c2ce6427987f7d2",
-    "zh:79bf8fb8f37c308742e287694a9de081ff8502b065a390d1bcfbd241b4eca203",
-    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
-    "zh:b7a5e1dfd9e179d70a169ddd4db44b56da90309060e27d36b329fe5fb3528e29",
-    "zh:c2cc728cb18ffd5c4814a10c203452c71f5ab0c46d68f9aa9183183fa60afd87",
-    "zh:c89bb37d2b8947c9a0d62b0b86ace51542f3327970f4e56a68bf81d9d0b8b65b",
-    "zh:ef2a61e8112c3b5e70095508aadaadf077e904b62b9cfc22030337f773bba041",
-    "zh:f714550b858d141ea88579f25247bda2a5ba461337975e77daceaf0bb7a9c358",
+    "h1:xu1ZOAgciMrxGk81qhnNmUG+yt6o4eq7qI8awc3/oLk=",
+    "zh:1e263d01a64740d550f14c83e91efaf5b3f7ee46163cce58b736da284e488377",
+    "zh:5b0885ee8875b98ef75f558e278ac5f2ba0850e0e6579c898d75a488be227e93",
+    "zh:6eb8d06a5cf9843e7a4dd18bd93fc785bdec9f5aebdef748bfbe349c0e085ce5",
+    "zh:82f57bcccc35271f39a090b687c37489f81ddeec2a792e7f5341c7cdcc51fa5c",
+    "zh:898d7ed728f45c3124e2bfbbde57b762e59c9c54e020453edc9454858d65a8c5",
+    "zh:8effa4a08cc3ffce2048b4e51ad6df0c288ce0c79c161b9716a16b482d2f18be",
+    "zh:ac80c44c9dc0c7016c3422390b17380ad03257abc09a224734ed359b2cd61d0a",
+    "zh:c2e03eba3d9af62948ba82aa1de627c692731b9a7dd46119b932c6fc9514306c",
+    "zh:ca77c67f72e7210112f485ef0fc555fde3ac818de9b7136f0b37142893ba6428",
+    "zh:ea8ccff7fece47be816f3f20fdd61b8ac3a72f515720c579a2560183b1e17658",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:f5d626eb6e9015022796849d6ba733627fa9e082302a8658dd83fc74c75db162",
   ]
 }
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,109 @@
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  token      = var.token
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "lexical-aquifer-445708-u1"
+  region      = "asia-southeast1"
+  credentials = "key.json"
+}
+
+resource "google_container_cluster" "autopilot_cluster_1" {
+  name               = "autopilot-cluster-1"
+  location           = "asia-southeast1-a" # Change to single zone
+  initial_node_count = 1 
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  network    = "projects/lexical-aquifer-445708-u1/global/networks/firstvpc"
+  subnetwork = "projects/lexical-aquifer-445708-u1/regions/asia-southeast1/subnetworks/public-subnet"
+
+  ip_allocation_policy {}
+  datapath_provider = "ADVANCED_DATAPATH"
+
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+    http_load_balancing {
+      disabled = false
+    }
+  }
+
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  logging_service    = "logging.googleapis.com/kubernetes"
+}
+
+resource "google_container_node_pool" "public_node_pool" {
+  name       = "public-node-pool"
+  cluster    = google_container_cluster.autopilot_cluster_1.name
+  location   = "asia-southeast1-a" # Match single zone
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    image_type   = "COS_CONTAINERD"
+    disk_type    = "pd-balanced"
+    disk_size_gb = 15
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+
+    service_account = "terraform-svc@lexical-aquifer-445708-u1.iam.gserviceaccount.com"
+  }
+
+  upgrade_settings {
+    max_surge       = 1
+    max_unavailable = 0
+  }
+}
+
+resource "google_container_node_pool" "private_node_pool" {
+  name       = "private-node-pool"
+  cluster    = google_container_cluster.autopilot_cluster_1.name
+  location   = "asia-southeast1-a" # Match single zone
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    image_type   = "COS_CONTAINERD"
+    disk_type    = "pd-balanced"
+    disk_size_gb = 15
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+
+    service_account = "terraform-svc@lexical-aquifer-445708-u1.iam.gserviceaccount.com"
+  }
+
+  upgrade_settings {
+    max_surge       = 1
+    max_unavailable = 0
+  }
 }
